1347a552c8179e61ce624b27cc052fdc7752c618
autonestarcontacto <autonest.ar.contacto@gmail.com>
2025-11-08 20:44:19 -0300

    Update RegistrationModal.tsx

--- components/RegistrationModal.tsx
+++ components/RegistrationModal.tsx
@@ -20,7 +20,8 @@ const InputField: React.FC<React.InputHTMLAttributes<HTMLInputElement> & { label
 const SelectField: React.FC<React.SelectHTMLAttributes<HTMLSelectElement> & { label: string, children: React.ReactNode }> = ({ label, children, ...props }) => (
      <div>
         <label className="block text-sm font-medium text-gray-700">{label}</label>
-        <select {...props} className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-autonest-blue focus:border-autonest-blue sm:text-sm rounded-md">
+        <select {...props} className="mt-1 block w-full pl-3 pr-10 py-2 text-base border border-gray-300 focus:outline-none focus:ring-autonest-blue
+  focus:border-autonest-blue sm:text-sm rounded-md">
             {children}
         </select>
     </div>
